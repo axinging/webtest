efc396cdfe0eb3f8aec134779dfb885563e3a104
Xu Xing <xing.xu@intel.com>
2021-02-07 12:13:29 +0800

    Format code with VSC

--- src/workloads/TFJS.js
+++ src/workloads/TFJS.js
@@ -1,11 +1,11 @@
 const settings = require('../../config.json');
 const platformBrowser = require('../browser.js');
-const { chromium } = require('playwright-chromium');
+const {chromium} = require('playwright-chromium');
 const path = require('path');
 const fs = require('fs');
 
 async function runTensorflowTest(workload, flags) {
-  let args = ["--start-maximized"];
+  let args = ['--start-maximized'];
   if (flags !== undefined) {
     args = args.concat(flags);
   }
@@ -19,21 +19,27 @@ async function runTensorflowTest(workload, flags) {
     args: args
   });
   const page = await browser.newPage();
-  browser.setDefaultNavigationTimeout( 3 * 60 * 1000 );
-  await page.goto(workload.url, { waitUntil: "networkidle" });
+  browser.setDefaultNavigationTimeout(3 * 60 * 1000);
+  await page.goto(workload.url, {waitUntil: 'networkidle'});
 
-  const modelsSelect = await page.$('#gui > ul > li:nth-child(1) > div > ul > li.cr.string > div > div > select');
+  const modelsSelect = await page.$(
+      '#gui > ul > li:nth-child(1) > div > ul > li.cr.string > div > div > select');
   if (workload.name.indexOf('ResNet_Tensor') > -1) {
-    await modelsSelect.type('posenet_resNet_q4_s32_input224_tensor', {delay: 100});
+    await modelsSelect.type(
+        'posenet_resNet_q4_s32_input224_tensor', {delay: 100});
   } else if (workload.name.indexOf('ResNet_Image') > -1) {
-    await modelsSelect.type('posenet_resNet_q4_s32_input224_image', {delay: 100});
+    await modelsSelect.type(
+        'posenet_resNet_q4_s32_input224_image', {delay: 100});
   } else if (workload.name.indexOf('MobileNet_Tensor') > -1) {
-    await modelsSelect.type('posenet_mobileNet_q2_m75_s16_input513_tensor', {delay: 100});
+    await modelsSelect.type(
+        'posenet_mobileNet_q2_m75_s16_input513_tensor', {delay: 100});
   } else if (workload.name.indexOf('MobileNet_Image') > -1) {
-    await modelsSelect.type('posenet_mobileNet_q2_m75_s16_input513_image', {delay: 100});
+    await modelsSelect.type(
+        'posenet_mobileNet_q2_m75_s16_input513_image', {delay: 100});
   }
 
-  const backendSelect = await page.$('#gui > ul > li:nth-child(4) > div > ul > li.cr.string > div > div > select');
+  const backendSelect = await page.$(
+      '#gui > ul > li:nth-child(4) > div > ul > li.cr.string > div > div > select');
   if (workload.name.indexOf('WASM') > -1) {
     await backendSelect.type('wasm', {delay: 100});
   } else if (workload.name.indexOf('WebGL') > -1) {
@@ -52,24 +58,31 @@ async function runTensorflowTest(workload, flags) {
   // parsing (since it's all backed by WebSockets) it still is taking up
   // time that could better be spent doing something else.
   await page.evaluate(async () => {
-    const runButton = document.querySelector('#gui > ul > li:nth-child(5) > div > span');
+    const runButton =
+        document.querySelector('#gui > ul > li:nth-child(5) > div > span');
     runButton.click();
     await new Promise(resolve => setTimeout(resolve, 30 * 1000));
   });
   // Waits for result elements
-  await page.waitForSelector('#timings > tbody > tr:nth-child(8) > td:nth-child(2)', { timeout: 10 * 60 * 1000 });
+  await page.waitForSelector(
+      '#timings > tbody > tr:nth-child(8) > td:nth-child(2)',
+      {timeout: 10 * 60 * 1000});
 
-  const resultElem = await page.$('#timings > tbody > tr:nth-child(8) > td:nth-child(2)');
+  const resultElem =
+      await page.$('#timings > tbody > tr:nth-child(8) > td:nth-child(2)');
   const result = await resultElem.evaluate(element => element.textContent);
   let results = {};
   console.log(`Result: ${result}`);
   results['Total Score'] = result;
 
   const resultBody = await page.$('#timings > tbody');
-  const resultLength = await resultBody.evaluate(element => element.rows.length);
-  for (let i = 1; i < resultLength; i ++) {
-    let typeSelector = `#timings > tbody > tr:nth-child(${i}) > td:nth-child(1)`;
-    let valueSelector = `#timings > tbody > tr:nth-child(${i}) > td:nth-child(2)`;
+  const resultLength =
+      await resultBody.evaluate(element => element.rows.length);
+  for (let i = 1; i < resultLength; i++) {
+    let typeSelector =
+        `#timings > tbody > tr:nth-child(${i}) > td:nth-child(1)`;
+    let valueSelector =
+        `#timings > tbody > tr:nth-child(${i}) > td:nth-child(2)`;
     const typeElem = await page.$(typeSelector);
     const valueElem = await page.$(valueSelector);
     const type = await typeElem.evaluate(element => element.innerText);
@@ -77,15 +90,12 @@ async function runTensorflowTest(workload, flags) {
     results[type] = value;
   }
 
-  //console.log('********** Detailed results: **********');
-  //console.log(results);
+  // console.log('********** Detailed results: **********');
+  // console.log(results);
 
   await browser.close();
 
-  return Promise.resolve({
-    date: Date(),
-    results: results
-  });
+  return Promise.resolve({date: Date(), results: results});
 }
 
 module.exports = runTensorflowTest;
